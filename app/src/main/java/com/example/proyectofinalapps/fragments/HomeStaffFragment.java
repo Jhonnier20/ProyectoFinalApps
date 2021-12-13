3df7ea5cfc2fe33726ce882c9757202fd23fcf00
juandavid2025 <juandavidaguirre2025@gmail.com>
2021-12-12 21:37:48 -0500

    notification staff and client payment

--- app/src/main/java/com/example/proyectofinalapps/fragments/HomeStaffFragment.java
+++ app/src/main/java/com/example/proyectofinalapps/fragments/HomeStaffFragment.java
@@ -2,6 +2,8 @@ package com.example.proyectofinalapps.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,14 +15,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.proyectofinalapps.activities.ActivateClient_AllowEntry;
 import com.example.proyectofinalapps.activities.Notifications;
 import com.example.proyectofinalapps.adapters.StaffAdapter;
 import com.example.proyectofinalapps.databinding.FragmentHomeStaffBinding;
 import com.example.proyectofinalapps.model.Person;
+import com.example.proyectofinalapps.model.Subscription;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
 
 public class HomeStaffFragment extends Fragment implements ActivateClient_AllowEntry.OnActivedClient {
 
@@ -72,6 +79,7 @@ public class HomeStaffFragment extends Fragment implements ActivateClient_AllowE
         notifications.setOnClickListener(this::goToNotifications);
         buscarBtn.setOnClickListener(this::searchClient);
 
+        watchClientPayment();
         chargeClients();
         return view;
     }
@@ -130,6 +138,60 @@ public class HomeStaffFragment extends Fragment implements ActivateClient_AllowE
                             adapter.addClient(person);
                             adapter.notifyDataSetChanged();
                         }
+                    }
+                }
+        );
+    }
+
+    private void watchClientPayment() {
+        FirebaseFirestore.getInstance().collection("Payments").addSnapshotListener(
+                (value, error) -> {
+
+                    for (DocumentChange dc: value.getDocumentChanges()){
+                     switch (dc.getType()){
+                         case ADDED:
+                             //esta persona acabo de pagar
+                             Person client = dc.getDocument().toObject(Person.class);
+
+                             AlertDialog.Builder builder = new AlertDialog.Builder(getContext())
+                                     .setTitle(client.getFullName()+" acaba de realizar pago")
+                                     .setMessage("¿Confirma el pago?")
+                                     .setNegativeButton("NO", (dialog, id) -> {
+                                         FirebaseFirestore.getInstance().collection("Payments").document(client.getId()).delete();
+                                         dialog.dismiss();
+                                     })
+                                     .setPositiveButton("SI", (dialog, id) -> {
+
+                                         FirebaseFirestore.getInstance().collection("Clientes").document(client.getId()).collection("Subscription").get().addOnSuccessListener(
+                                                 sussSub -> {
+                                                     for(DocumentSnapshot doc: sussSub.getDocuments()) {
+                                                         Subscription sub = doc.toObject(Subscription.class);
+
+                                                         sub.setActive(true);
+                                                         sub.setDateStart(new Date().getTime());
+                                                         long dateend = new Date().getTime() + 2147483647;
+                                                         dateend += 432000000;
+                                                         sub.setDateEnd(dateend);
+                                                         sub.setMembership("Gold");
+                                                         sub.setState("Pago");
+
+                                                         FirebaseFirestore.getInstance().collection("Clientes").document(client.getId()).collection("Subscription").document(sub.getId()).set(sub).addOnSuccessListener(
+                                                                 suss -> {
+                                                                     FirebaseFirestore.getInstance().collection("Payments").document(client.getId()).delete();
+                                                                     Toast.makeText(getActivity(),"Persona activada", Toast.LENGTH_LONG).show();
+                                                                 }
+                                                         );
+                                                     }
+                                                 }
+                                         );
+
+                                         dialog.dismiss();
+                                     });
+                             builder.show();
+
+                             Toast.makeText(getContext(), "Alguien agrego algo", Toast.LENGTH_LONG).show();
+                             break;
+                     }
                     }
                 }
         );
